c1f59a008d6f3f8ff22d36f41c61848a315b6066
pslebedev <88189199+pslebedev@users.noreply.github.com>
2022-07-14 23:14:34 +0300

    Update homework9.spec.js

--- specs/homework9.spec.js
+++ specs/homework9.spec.js
@@ -2,26 +2,26 @@ import {token, email,empty, notValid, notSupplied, witoutApiKey} from '../framew
 import api from '../framework/services/index.js'
 
 describe('Validates and verifies an email address ', () => {
-//     test('measuring deliverability and quality /GET 200', async () => {
-//       const response = await api().Email().get(token, email);
-//       expect(response.status).toEqual(200);
-//        });
+    test('measuring deliverability and quality /GET 200', async () => {
+      const response = await api().Email().get(token, email);
+      expect(response.status).toEqual(200);
+       });
     
-//      test.each`
-//      email                       | expected
-//        ${''}                        | ${empty}
-//        ${'qwerty@'}                     | ${notValid}     
-//      `('$email = $expected', async ({email, expected}) => {
-//        const response = await api().Email().get(token, email);
-//        let data = await response.json();
-//        expect(data).toEqual(expected);
-//        expect(response.status).toEqual(200);
-//      });  
+     test.each`
+     email                       | expected
+       ${''}                        | ${empty}
+       ${'qwerty@'}                     | ${notValid}     
+     `('$email = $expected', async ({email, expected}) => {
+       const response = await api().Email().get(token, email);
+       let data = await response.json();
+       expect(data).toEqual(expected);
+       expect(response.status).toEqual(200);
+     });  
 
-//      test('measuring deliverability without apikey /GET 200', async () => {
-//      const response = await api().Email().get('', email);
-//      expect(response.status).toEqual(401);
-//      let data = await response.json();
-//      expect(data).toEqual(witoutApiKey);
-//          });
+     test('measuring deliverability without apikey /GET 200', async () => {
+     const response = await api().Email().get('', email);
+     expect(response.status).toEqual(401);
+     let data = await response.json();
+     expect(data).toEqual(witoutApiKey);
+         });
     })
